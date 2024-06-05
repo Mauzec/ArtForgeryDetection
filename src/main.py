864f0cae1f1096a143fe8389ff21741a7bec1712
Victor <kolos.130278@gmail.com>
2024-06-05 13:26:05 +0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -12,32 +12,6 @@ with open('config.yaml', 'r') as config:
     cfg = yaml.safe_load(config)
     
 PATH = cfg['Victor']['Dataset']
-
-
-
-def main(percentage: int, descriptor) -> int:
-    DatasetOperations.clear()
-    DatasetOperations.get_mona_original(PATH=PATH)
-    DatasetOperations.get_work_train_dataset(PATH=PATH, percentage_train=percentage)
-    DatasetOperations.get_mona_test(PATH=PATH)
-    DatasetOperations.scale_all()
-    
-    bovw = BoVW(descriptor=descriptor, number_words=500)
-    
-    bovw.add_train_dataset("dataset/train")
-    
-    bovw.model_training()
-    
-    
-    result = np.array([
-        bovw.classification_image("dataset/train/artist/mona_original.png"),
-        bovw.classification_image("dataset/test/artist/mona_younger_1.jpg"),
-        bovw.classification_image("dataset/test/artist/mona_younger_2.jpg"),
-        bovw.classification_image("dataset/test/artist/mona_younger_3.jpg"),
-        bovw.classification_image("dataset/test/artist/mona_younger_4.jpg"),
-    ])
-    print(result)
-    DatasetOperations.clear()
     
 def add_train_dataset(percentage: int = 100):
     DatasetOperations.clear()
